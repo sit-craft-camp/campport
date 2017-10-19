6ba3d6cfacc400895b4d7b1cd6b6a795089105ea
Pittawat <pittawat2542@hotmail.com>
2017-10-19 15:02:35 +0700

    Add RequirementCard.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import HeroImage from '../components/HeroImage'
+import RequirementCard from '../components/RequirementCard'
 
 export const Index = () => (
   <div>
-    <HeroImage src="https://pixabay.com/get/eb3db30c21f0083ed65e4518a34e4796e077e5d004b014419df3c87fa1e8b7/seemed-2823949_640.jpg" />
+    <RequirementCard requirements={["ม.5-6","สายการเรียน วิทย์-คณิต"]} />
   </div>
 )
 
