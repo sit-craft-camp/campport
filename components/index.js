367ef376347b18bfc64f558bbc50e1a65f689ee7
Jasonnonstop10 <jay44411@gmail.com>
2017-10-20 15:42:03 +0700

    add review

--- components/index.js
+++ components/index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
+import ReviewCard from './reviewCard'
+import Review from './review'
+=======
 import ReviewCard from './reviewCard/reviewCard'
 
+>>>>>>> b614290372b3d19a8584a944cd3837ce3a1974b7
 export {
-  ReviewCard,
+  ReviewCard,Review,
 }
