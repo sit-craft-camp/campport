702b121c522a87a047932d0fc3b0dab7027cbcec
Jasonnonstop10 <jay44411@gmail.com>
2017-10-20 15:38:38 +0700

    add index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 <<<<<<< HEAD
+import { Review } from '../components'
+
+export const Index = () => (
+  <div>
+    <Review />
+=======
+<<<<<<< HEAD
 // import Component from '../components/'
 // import Navbar from '../components/Navbar'
 
@@ -15,6 +22,7 @@ export const Index = () => (
   <div>
     <ReviewCard date="14 oct 2017" userImage="https://scontent.fbkk14-1.fna.fbcdn.net/v/t1.0-9/22195377_1725635537741020_6828151920495242605_n.jpg?oh=316e6494728d85233f8a6201706da18c&oe=5A6D7D97" userName="DOMMIE" star={4} comment="Hello It's me" />
 >>>>>>> 2853c079ce9e7c0dbaeb6b3dbff01a1f3fd46ed9
+>>>>>>> b614290372b3d19a8584a944cd3837ce3a1974b7
   </div>
 )
 
